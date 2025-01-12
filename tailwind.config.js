feba4be0abc2d9676e726560a9cf6bebe192f300
Yuvraj singh <yuvrajs2122005@gmail.com>
2025-01-12 15:59:32 +0530

    update content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: [
+    "./*.html",          // Matches all HTML files in the root folder
+    "./**/*.html",       // Matches all HTML files in subdirectories
+    "./*.js",            // Matches all JavaScript files in the root folder
+    "./**/*.js",         // Matches all JavaScript files in subdirectories
+  ],
   theme: {
     extend: {},
   },
